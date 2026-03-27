e8ac14b18eab818575587ef6ad43a6fd34e3647b
ishannilaksha <ishanhatharasinghe222@gmail.com>
2026-03-27 11:59:48 +0530

    Updates

--- assets/1 (19)-CV5xT-kq.js
+++ assets/1 (19)-CV5xT-kq.js
@@ -1,4 +1,4 @@
-import{c as s}from"./index-CjZW2xEJ.js";/**
+import{c as s}from"./index-Dvs6cUgB.js";/**
  * @license lucide-react v0.473.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/arrow-right-Dg_GXZxV.js
+++ assets/arrow-right-Dg_GXZxV.js
@@ -1,4 +1,4 @@
-import{c as o}from"./index-CjZW2xEJ.js";/**
+import{c as o}from"./index-Dvs6cUgB.js";/**
  * @license lucide-react v0.473.0 - ISC
  *
  * This source code is licensed under the ISC license.
